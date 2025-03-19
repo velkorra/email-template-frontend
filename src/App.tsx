7b5d4bda096eb39267300b173e9011d134120314
velkorra <anton.s.vertunov@gmail.com>
2025-03-20 01:21:24 +0300

    use vite instead of react scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, createBrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import MainPage from "./pages/MainPage";
 import { useAuth } from "./context/AuthContext";
 import Layout from "./layout/Layout";
 import Main from "./layout/Main";
@@ -11,7 +10,7 @@ import Settings from "./layout/Settings";
 import ConstructorPage from "./components/ConstructorPage";
 
 const App: React.FC = () => {
-  const { isAuthenticated, isLoading } = useAuth();
+  const { isLoading } = useAuth();
   if (isLoading) {
     return null;
   }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -10,8 +8,8 @@ import "@fontsource/roboto/700.css";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
 import { TemplateProvider } from "./context/TemplateContext";
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -22,8 +20,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
